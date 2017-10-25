21e8a68be695580ca4283cc3bf40f3c50aed0e31
ferreira <ferreira@NICKERSON>
2017-10-24 21:00:26 -0300

    modificando

--- src/appgitteste/AppGitTeste.java
+++ src/appgitteste/AppGitTeste.java
@@ -16,6 +16,9 @@ public class AppGitTeste {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("TESTANDO O GIT");
+        
     }
     
 }
